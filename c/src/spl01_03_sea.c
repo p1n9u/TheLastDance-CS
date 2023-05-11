e1ce94bee4b794bbadb70ee83cd1bd7bd7da67ae
p1n9u <cybrphntm4@realtimetech.co.kr>
2023-05-11 15:40:54 +0900

    SPLab: week01

---
+++ c/src/spl01_03_sea.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("from sea to\n");
+    printf("shining \nC\n");
+    return 0;
+}
---
+++ c/src/spl01_05_hippo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int hippo;
+    float bolt, phelps;
+
+    hippo = 50;
+    bolt = 44.7f;
+    printf("hippo - bolt = %f.\n", hippo - bolt);
+
+    phelps = 9.6f;
+    hippo = 9;
+    printf("phelps - hippo = %f.\n", phelps - hippo);
+
+    return 0;
+}
