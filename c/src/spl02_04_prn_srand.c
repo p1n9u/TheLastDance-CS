8b628f65da8f6ab2f0937d5c5674fb731ac0db96
p1n9u <cybrphntm4@realtimetech.co.kr>
2023-05-11 17:23:44 +0900

    SPLab: week02

---
+++ c/src/spl02_04_prn_srand.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    int i, n, seed;
+    seed = time(NULL);
+    srand(seed);
+    printf("\n%s\n%s",
+           "Some randomly distrubuted integers will be printed.",
+           "How many do you want to See? ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        if (i % 10 == 0)
+            putchar('\n');
+        printf("%12d", rand());
+    }
+    printf("\n\n");
+
+    return 0;
+}
